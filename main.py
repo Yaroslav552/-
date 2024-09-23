8a58aaa8978819cc54a5cc2c1b90983bff0489ae
Yaroslav552 <boywithoutmood@gmail.com>
2024-09-23 12:07:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 Number_of_completed_homework_assignments = "12"
 Number_of_hours_spent = "1.5"
 Course_title = "Python"
-Time_for_one_task = "0.125"
+Time_for_one_task = (1.5/12)
 print("Курс",Course_title,"всего задач",Number_of_completed_homework_assignments,"затрачено часов",Number_of_hours_spent,"среднее время выполнения",Time_for_one_task,"часа")
