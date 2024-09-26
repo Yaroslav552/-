5658f771d6e01783cab9d4da71b1add93d35bc0d
Yaroslav552 <boywithoutmood@gmail.com>
2024-09-26 18:17:34 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-my_string = input('Здраствуйте, как вас зовут? ')
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0:1])
-print(my_string[-1:10])
+my_dict = {'Yaroslav': 2002, 'Anton': 2003, 'Andrey': 1997}
+print(my_dict)
+print(my_dict['Yaroslav'])
+print(my_dict.get('Anna'))
+my_dict.update({'Yarik': 2004,
+                'Slava': 2008})
+print(my_dict['Slava'])
+del my_dict['Slava']
+print(my_dict)
+my_set = [1, 2, 3, 4, 1, 'apple', True]
+my_set = set(my_set)
+print(my_set)
+print(my_set.add(5))
+print(my_set.add('orange'))
+print(my_set)
+print(my_set.discard('apple'))
+print(my_set)
+
