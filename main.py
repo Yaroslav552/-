6d5b27ae9fe1107ef1ed0bc9a5dfb2b611fc36ec
Yaroslav552 <boywithoutmood@gmail.com>
2024-10-01 17:40:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-pystoi_slovar = {}
-names = list(students)
-names.sort()
-print(names[0])
-pystoi_slovar.update({names[0]:sum(grades[0])/len(grades[0]),
-                      names[1]:sum(grades[1])/len(grades[1]),
-                      names[2]: sum(grades[2]) / len(grades[2]),
-                      names[3]: sum(grades[3]) / len(grades[3]),
-                      names[4]: sum(grades[4]) / len(grades[4])
-                      })
-print(pystoi_slovar)
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
