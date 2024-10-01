33f5b1638904ac81e858c550c33dbc66ede1e83a
Yaroslav552 <boywithoutmood@gmail.com>
2024-10-01 19:56:30 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+first = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
+if first == second == third:
+    print('3')
+elif first == second or second == third or first == third:
+    print('2')
+elif first != second or second != third or first != third:
+    print('0')
