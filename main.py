ac94d7d001074e18b4b59c1008cf9243a0707e9e
Yaroslav552 <boywithoutmood@gmail.com>
2024-10-04 20:56:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-count = 0
-while count < len(my_list):
-    if  my_list[count] > 0:
-        print(my_list[count])
-        count = count + 1
-    elif my_list[count] == 0:
-        count += 1
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+for i in numbers:
+    if i < 2:
+        not_primes.append(i)
         continue
-    else:break
 
+    is_prime = True
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            is_prime = False
+            break
 
-
-
-
-
-
-
-
-
-
+    if is_prime:
+        primes.append(i)
+    else:
+        not_primes.append(i)
+print(f'Простые числа: {primes}')
+print(f'Непростые числа: {not_primes}')
